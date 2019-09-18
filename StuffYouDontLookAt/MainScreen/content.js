99865aef2ab449772ed2c09b61f4436eb66c252e
LilKalebFTW2 <54673099+LilKalebFTW2@users.noreply.github.com>
2019-09-18 11:36:46 -0400

    Delete content.js

--- StuffYouDontLookAt/MainScreen/content.js
+++
@@ -1,9 +0,0 @@
-function Play(){
-    location.href = "StuffYouDontLookAt/LevelOne/LevelOne.html";
-}
-function Options(){
-    location.href = "StuffYouDontLookAt/MainScreen/Options.html";
-}
-function Credits(){
-    location.href = "StuffYouDontLookAt/MainScreen/Credits.html";
-}
